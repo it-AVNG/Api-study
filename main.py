573952891c71c34edbe0ebe7c8ea0e22c9d6c240
An Vu Nguyen <it.avng@gmail.com>
2024-02-01 11:30:09 +0700

    add new api for POST method

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/posts")
+def get_post():
+    return {"data": "this is a post"}
+
+
+@app.post("/create-post")
+def create_post(new_post: Post):
+    print(new_post)
+    return {"data": new_post}
+
+
+# schema: title str, content str, category, publish bool
